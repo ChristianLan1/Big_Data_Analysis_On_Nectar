310d2884998b7f2a75336fa423990510677395f0
Terry Liao <jinliang@student.unimelb.edu.au>
2019-05-14 20:05:44 +1000

    mod tweet harvester

--- tweet_havester/tweet_havester.py
+++ tweet_havester/tweet_havester.py
@@ -1,11 +1,17 @@
 import tweepy_search as tSearch
 import tweepy_stream as tStream
 import time
+import sys
 
 if __name__ == "__main__":
-    server_path = 'http://admin:password@127.0.0.1:5984/'
-
+    server_path = ""
+    a=sys.argv
+    if(len(a) == 4):
+        ip = a[1]
+        username = a[2] 
+        password = a[3]
+        server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
     tStream.run(server_path)
     # wait for streamming for a while to start searching
-    time.sleep(200)
+    # time.sleep(200)
     tSearch.run(server_path)
